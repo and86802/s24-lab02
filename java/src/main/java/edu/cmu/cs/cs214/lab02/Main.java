4218fe6b6e676f0ed812422d7a0ce163618d58d3
and86802 <and86804@gmail.com>
2024-01-22 12:32:16 -0500

    update Renderer and Main to accommodate changes in Shape interface

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,18 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
-
+        Shape rectangle = new Rectangle(2, 10);
+        
         Renderer renderer = new Renderer(rectangle);
-
+        
         renderer.draw();
+
+        // Shape circle = new Circle(3);
+        // Renderer renderer_cir = new Renderer(circle);
+        // renderer_cir.draw();
+        
     }
 }
--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -2,18 +2,18 @@ package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.*;
 
-public class Renderer {
-    public Rectangle rectangle;
+public class Renderer implements Shape{
+    Shape shape;
     
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+    Renderer(Shape shape) {
+        this.shape = shape;
+    }
+
+    public double getArea(){
+        return this.shape.getArea();
     }
 
     void draw() {
-        double area = rectangle.getArea();
-
-        // assume implementation
-
-        System.out.println("Shape printed\n" + "Its area is " + area);
+        System.out.println("Shape printed\n" + "Its area is " + getArea());
     }
 }
